2b91fee556a31e5b006d432546c3c91fbdb01163
Harshita <harshitadlw16@gmail.com>
2026-04-02 03:39:31 +0530

    refactor: clean structure, add ProofOfSkill contract, remove boilerplate

--- src/genlayer-project-boilerplate/frontend/.next/dev/server/chunks/ssr/eae93_yer-project-boilerplate_frontend__next-internal_server_app_page_actions_36ea87b6.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-"[project]/OneDrive/Documents/GitHub/Proofed-Aleph-/src/genlayer-project-boilerplate/frontend/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__) => {
-"use strict";
-
-__turbopack_context__.s([]);
-}),
-];
-
-//# sourceMappingURL=eae93_yer-project-boilerplate_frontend__next-internal_server_app_page_actions_36ea87b6.js.map
--- src/genlayer-project-boilerplate/frontend/.next/dev/static/chunks/cfa53_@noble_curves_esm_secp256k1_b4f7b673.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/cfa53_@noble_curves_75c05d74._.js"
-],
-    source: "dynamic"
-});

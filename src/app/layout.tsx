0f4298c9627a06871920991f5fb8fe8809493682
Harshita <harshitadlw16@gmail.com>
2026-05-06 17:32:11 +0530

    Corrections in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
-'use client';
-
-import { WagmiProvider } from 'wagmi'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { config } from '@/lib/wagmi.config'
 import type { Metadata } from 'next';
 import { Providers } from './providers';
-import './globals.css' 
-
-const queryClient = new QueryClient()
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Proofed Protocol',
@@ -18,12 +11,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <WagmiProvider config={config}>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
-        </WagmiProvider>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
